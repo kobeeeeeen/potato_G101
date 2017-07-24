577a34b5b5becd9b4edb010ad395f29cd30d5917
Aachan0621 <30419683+Aachan0621@users.noreply.github.com>
2017-07-25 06:19:34 +0900

    ii
    iii

---
+++ stgtama.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class stgtama : MonoBehaviour {
+
+	float bulletSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(this.gameObject, 3);
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0, 0, bulletSpeed);
+
+		Destroy(gameObject, 10.5f);
+	}
+}
